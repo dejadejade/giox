72283e56d687f30e9fd9e69e172e1e717ec9c5f2
Deja <dejadejade9@outlook.com>
2020-06-16 13:03:06 -0700

    cleanup

--- fn/fn.go
+++ fn/fn.go
@@ -7,16 +7,11 @@ import (
 	"image/color"
 
 	"gioui.org/f32"
-	"gioui.org/gesture"
-	"gioui.org/io/key"
-	"gioui.org/io/pointer"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
 )
 
 type (
@@ -33,50 +28,10 @@ func Styled(w layout.Widget, styles ...Style) layout.Widget {
 	return w
 }
 
-func Margin(x float32) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			return layout.UniformInset(unit.Dp(x)).Layout(gtx, w)
-		}
-	}
-}
-
-func Margin4(left, top, right, bottom float32) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			in := layout.Inset{Top: unit.Dp(top), Right: unit.Dp(right), Left: unit.Dp(left), Bottom: unit.Dp(bottom)}
-			return in.Layout(gtx, w)
-		}
-	}
-}
-
 func Size(width, height float32) Style {
 	return func(w layout.Widget) layout.Widget {
 		return func(gtx C) D {
-			cs := gtx.Constraints
-			if width > 0 {
-				ww := gtx.Px(unit.Dp(width))
-				if ww < cs.Min.X {
-					ww = cs.Min.X
-				}
-				if ww > cs.Max.X {
-					ww = cs.Max.X
-				}
-				gtx.Constraints.Min.X = ww
-				gtx.Constraints.Max.X = ww
-			}
-			if height > 0 {
-				hh := gtx.Px(unit.Dp(height))
-				if hh < cs.Min.Y {
-					hh = cs.Min.Y
-				}
-				if hh > cs.Max.Y {
-					hh = cs.Max.Y
-				}
-				gtx.Constraints.Min.Y = hh
-				gtx.Constraints.Max.Y = hh
-			}
-			return w(gtx)
+			return sizeS{width, height}.Layout(gtx, w)
 		}
 	}
 }
@@ -113,14 +68,6 @@ func (s sizeS) Layout(gtx C, w layout.Widget) D {
 	return w(gtx)
 }
 
-func Direction(d layout.Direction) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			return d.Layout(gtx, w)
-		}
-	}
-}
-
 func drawRect(ops *op.Ops, x, y, w, h float32, fillcolor color.RGBA) {
 	r := f32.Rect(x, y+h, x+w, y)
 	paint.ColorOp{Color: fillcolor}.Add(ops)
@@ -130,29 +77,7 @@ func drawRect(ops *op.Ops, x, y, w, h float32, fillcolor color.RGBA) {
 func Border(left, top, right, bottom float32, col color.RGBA) Style {
 	return func(w layout.Widget) layout.Widget {
 		return func(gtx C) D {
-			m := op.Record(gtx.Ops)
-			dims := w(gtx)
-			call := m.Stop()
-
-			ops := gtx.Ops
-
-			defer op.Push(gtx.Ops).Pop()
-			w, h := float32(dims.Size.X), float32(dims.Size.Y)
-			if left > 0 {
-				drawRect(ops, 0, 0, left, h, col)
-			}
-			if top > 0 {
-				drawRect(ops, 0, 0, w, top, col)
-			}
-			if right > 0 && w > right {
-				drawRect(ops, w-right, 0, right, h, col)
-			}
-			if bottom > 0 && h > bottom {
-				drawRect(ops, 0, h-bottom, w, bottom, col)
-			}
-
-			call.Add(gtx.Ops)
-			return dims
+			return borderS{left, top, right, bottom, col}.Layout(gtx, w)
 		}
 	}
 }
@@ -190,36 +115,6 @@ func (s borderS) Layout(gtx C, widget layout.Widget) D {
 	return dims
 }
 
-func Visible(v bool) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			if !v {
-				return layout.Dimensions{}
-			}
-			return w(gtx)
-		}
-	}
-}
-
-func OnClick(click *gesture.Click) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			dims := w(gtx)
-			pointer.Rect(image.Rectangle{Max: dims.Size}).Add(gtx.Ops)
-			click.Add(gtx.Ops)
-			return dims
-		}
-	}
-}
-
-func Clickable(btn *widget.Clickable) Style {
-	return func(w layout.Widget) layout.Widget {
-		return func(gtx C) D {
-			return material.Clickable(gtx, btn, w)
-		}
-	}
-}
-
 func FillRect(col color.RGBA, sz image.Point) layout.Widget {
 	return func(gtx C) D {
 		w := gtx.Px(unit.Dp(float32(sz.X)))
@@ -250,15 +145,6 @@ func Fill(col color.RGBA) layout.Widget {
 	}
 }
 
-func Background(col color.RGBA) Style {
-	return func(w layout.Widget) layout.Widget {
-		return Stack(layout.Stack{}, nil,
-			StackChild{Expanded(true), nil, Fill(col)},
-			StackChild{Stacked(), nil, w},
-		)
-	}
-}
-
 type backgroundS struct {
 	col color.RGBA
 }
@@ -268,107 +154,6 @@ func (s backgroundS) Layout(gtx C, w layout.Widget) D {
 		layout.Expanded(Fill(s.col)),
 		layout.Stacked(w),
 	)
-}
-
-func Rigid(expand bool) FlexChildFunc {
-	return func(w layout.Widget) layout.FlexChild {
-		return layout.Rigid(func(gtx C) D {
-			if expand {
-				gtx.Constraints.Min.X = gtx.Constraints.Max.X
-			}
-			return w(gtx)
-		})
-	}
-}
-
-func Flexed(i float32) FlexChildFunc {
-	return func(w layout.Widget) layout.FlexChild {
-		return layout.Flexed(i, func(gtx C) D {
-			gtx.Constraints.Min.X = gtx.Constraints.Max.X
-			return w(gtx)
-		})
-	}
-}
-
-type FlexChildFunc func(w layout.Widget) layout.FlexChild
-type FlexChild struct {
-	Wrapper FlexChildFunc
-	Styles  []Style
-	Widget  layout.Widget
-}
-
-type StackChildFunc func(w layout.Widget) layout.StackChild
-type StackChild struct {
-	Wrapper StackChildFunc
-	Styles  []Style
-	Widget  layout.Widget
-}
-
-func Flex(flex layout.Flex, styles []Style, children ...FlexChild) layout.Widget {
-	w := func(gtx C) D {
-		var widgets []layout.FlexChild
-		for _, child := range children {
-			widget := child.Wrapper(Styled(child.Widget, child.Styles...))
-			widgets = append(widgets, widget)
-		}
-		return flex.Layout(gtx, widgets...)
-	}
-
-	return Styled(w, styles...)
-}
-
-func Stack(stack layout.Stack, styles []Style, children ...StackChild) layout.Widget {
-	w := func(gtx C) D {
-		var widgets []layout.StackChild
-		for _, child := range children {
-			widget := child.Wrapper(Styled(child.Widget, child.Styles...))
-			widgets = append(widgets, widget)
-		}
-		return stack.Layout(gtx, widgets...)
-	}
-
-	return Styled(w, styles...)
-}
-
-func Expanded(expand bool) StackChildFunc {
-	return func(w layout.Widget) layout.StackChild {
-		return layout.Expanded(func(gtx C) D {
-			if expand {
-				gtx.Constraints.Min.X = gtx.Constraints.Max.X
-			}
-			return w(gtx)
-		})
-	}
-}
-
-func Stacked() StackChildFunc {
-	return func(w layout.Widget) layout.StackChild {
-		return layout.Stacked(func(gtx C) D {
-			return w(gtx)
-		})
-	}
-}
-
-func List(list *layout.List, styles []Style, n int, child layout.ListElement) layout.Widget {
-	w := func(gtx C) D {
-		if list.Dragging() {
-			key.HideInputOp{}.Add(gtx.Ops)
-		}
-		return list.Layout(gtx, n, child)
-	}
-
-	return Styled(w, styles...)
-}
-
-func Label(l material.LabelStyle) layout.Widget {
-	return l.Layout
-}
-
-type LabelOption func(l material.LabelStyle)
-
-func LabelFont() LabelOption {
-	return func(l material.LabelStyle) {
-	}
 }
 
 func Rounded(r float32) Style {
